8706ced9202381fd03a7b8f1474b05e79b7c8639
dots225 <79728151+dots225@users.noreply.github.com>
2021-02-26 18:38:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ for (const file of commandFiles) {
 client.once('ready', () => {
     client.user.setActivity(`${config.prefx}match <beatmap> | ‎‎currently finding all 1-2 maps...`);
     console.log("Ready!");
-    console.log("https://discord.com/oauth2/authorize?client_id=814919450993754172&scope=bot");
+    console.log("https://discord.com/oauth2/authorize?client_id=CLIENTIDGOESHERE&scope=bot");
 })
 
 client.on('message', async message => {
